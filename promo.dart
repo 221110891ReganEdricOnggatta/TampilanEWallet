6f8d8393a8908022b66119b07cf66e01bc29bbd1
CalvinWijaya0 <221112275@students.mikroskil.ac.id>
2024-07-07 22:20:41 +0700

    Add files via upload

---
+++ promo.dart
@@ -0,0 +1,28 @@
+class Promo {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+
+  Promo({required this.imageUrl, required this.title, required this.subtitle});
+}
+
+final List<Promo> promos = [
+  Promo(
+    imageUrl:
+        'https://d2vuyvo9qdtgo9.cloudfront.net/promos/May2024/SIMhyDa6KFu8nB8kPD4H.jpg',
+    title: 'Banana McFlurry & Choco Caramel Pie',
+    subtitle: 'Nikmati Dessert Baru Favoritmu',
+  ),
+  Promo(
+    imageUrl:
+        'https://d2vuyvo9qdtgo9.cloudfront.net/promos/June2024/WPkV01ySiP2bWgXZt1KB.jpg',
+    title: 'Banana McFlurry & Choco Caramel Pie',
+    subtitle: 'Nikmati Dessert Baru Favoritmu',
+  ),
+  Promo(
+    imageUrl:
+        'https://d2vuyvo9qdtgo9.cloudfront.net/promos/June2024/rdbudBkw3QPESlSHyXxB.jpg',
+    title: 'Banana McFlurry & Choco Caramel Pie',
+    subtitle: 'Nikmati Dessert Baru Favoritmu',
+  ),
+];
