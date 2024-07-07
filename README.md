225088e0c76de970a59f09b6cc91b669bc9497d2
CalvinWinata <165750935+221111448CalvinWinata@users.noreply.github.com>
2024-07-07 22:35:32 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-1. Menu login
-     ![WhatsApp Image 2024-07-07 at 22 29 55_9ca9822f](https://github.com/221110891ReganEdricOnggatta/TampilanEWallet/assets/165750935/53b24f64-6a51-4c47-99da-0365cd08cf07)
-2. Menu Cicilan
-3. atoms yang berisi ListTopUp
-   ![WhatsApp Image 2024-07-07 at 22 33 08_9754f46b](https://github.com/221110891ReganEdricOnggatta/TampilanEWallet/assets/165750935/feec43e0-5008-4487-b198-d655a2e1fd64)
+# dana_kedua
 
-4. Menu User
-   
-5. Menu History
-   ![WhatsApp Image 2024-07-07 at 22 29 56_9322a484](https://github.com/221110891ReganEdricOnggatta/TampilanEWallet/assets/165750935/b507f031-6753-4435-9cb8-2c1e59e20975)
+A new Flutter project.
 
-   
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+# TampilanEWallet
---
+++ drawer.dart
@@ -0,0 +1,144 @@
+import 'package:dana_kedua/providers/user.dart';
+import 'package:dana_kedua/screens/profile/profile.dart';
+import 'package:dana_kedua/screens/settings.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+    final loggedInUser = userProvider.loggedInUser;
+
+    return Drawer(
+      backgroundColor: Colors.grey[200],
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.grey[800]),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: userProvider.profileImage != null
+                      ? FileImage(userProvider.profileImage!)
+                      : const AssetImage('assets/placeholder.png') as ImageProvider,
+                  child: userProvider.profileImage == null
+                      ? const Icon(Icons.person, size: 50, color: Colors.white)
+                      : null,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        loggedInUser?.username ?? "Guest",
+                        style: const TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        loggedInUser?.email ?? "Guest",
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w100,
+                            fontSize: 12),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) => const MyProfiles()),
+                    );
+                  },
+                  child: const Text("Edit"),
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            thickness: 1,
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.home),
+            title: const Text("Home"),
+            trailing: const Icon(Icons.arrow_right),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.history),
+            title: const Text("History Transaction"),
+            trailing: const Icon(Icons.arrow_right),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.contacts),
+            title: const Text("Contacts"),
+            trailing: const Icon(Icons.arrow_right),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.payment),
+            title: const Text("Payment"),
+            trailing: const Icon(Icons.arrow_right),
+          ),
+          const SizedBox(
+            height: 160,
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const MySettings()));
+            },
+            leading: const Icon(Icons.settings),
+            title: const Text("Settings"),
+          ),
+          ListTile(
+            onTap: () {
+              _showLogoutDialog(context);
+            },
+            leading: const Icon(Icons.logout),
+            title: const Text("Log out"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Konfirmasi"),
+        content: const Text("Apakah Anda yakin ingin Logout?"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Tidak"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/', (route) => false);
+            },
+            child: const Text("Ya"),
+          ),
+        ],
+      ),
+    );
+  }
+}
